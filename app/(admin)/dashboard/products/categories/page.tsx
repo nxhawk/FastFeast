32538e80640ab89dfa9a5c5bf1310e9725350cf6
21120447 <haonhat2729@gmail.com>
2024-09-28 23:12:22 +0700

    :sparkles: feat: change call api

--- app/(admin)/dashboard/products/categories/page.tsx
+++ app/(admin)/dashboard/products/categories/page.tsx
@@ -1,8 +1,8 @@
 import { type Metadata } from "next";
 import React from "react";
-import { type Category } from "@prisma/client";
 import CategoryHeader from "@/components/admin/category/category-header";
 import CategoryTable from "@/components/admin/category/category-table";
+import { listCategories } from "@/models/category";
 
 export const metadata: Metadata = {
   title: "Danh mục Sản phẩm - Fast Food Ordering website",
@@ -10,16 +10,8 @@ export const metadata: Metadata = {
   icons: "images/logo.png",
 };
 
-const getListCategory = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_API}/category`, {
-    next: { tags: ["categories"] },
-  });
-  const categories = await res.json();
-  return categories.data as Category[];
-};
-
 const Page = async () => {
-  const categories = await getListCategory();
+  const categories = await listCategories();
   return (
     <div>
       <CategoryHeader />
--- app/api/category/route.ts
+++ app/api/category/route.ts
@@ -1,24 +1,8 @@
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import { NextResponse } from "next/server";
-import { revalidateTag } from "next/cache";
-import { listCategories, newCategory } from "@/models/category";
+// import { revalidateTag } from "next/cache";
+import { newCategory } from "@/models/category";
 import { getErrorMessage } from "@/utils/helper";
-
-export async function GET() {
-  try {
-    const categories = await listCategories();
-    return NextResponse.json({ message: "success", data: categories });
-  } catch (error) {
-    console.log("Error getting all categories", getErrorMessage(error));
-
-    return NextResponse.json(
-      { message: `Internal Server Error: ${getErrorMessage(error)}` },
-      {
-        status: 500,
-      },
-    );
-  }
-}
 
 export async function POST(request: Request) {
   try {
@@ -34,7 +18,7 @@ export async function POST(request: Request) {
     }
 
     const nCategory = await newCategory(name as string, (slug as string).toLowerCase());
-    revalidateTag("categories");
+    // revalidateTag("categories");
     return NextResponse.json({ message: "success", data: nCategory });
   } catch (error) {
     if (error instanceof SyntaxError) {
