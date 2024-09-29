53b65b4e9ae582c77be1eff73da513117262643f
21120447 <haonhat2729@gmail.com>
2024-09-30 00:01:28 +0700

    :recycle: refactor: mobile responsive

--- components/user/list-category/index.tsx
+++ components/user/list-category/index.tsx
@@ -10,13 +10,13 @@ const ListCategory = ({ categories }: Props) => {
   return (
     <div className="fixed top-[65px] left-0 w-full bg-white">
       <div className="bg-red-50">
-        <div className="max-w-screen-lg mx-auto py-2 px-1">
+        <div className="max-w-screen-lg max-md:text-sm mx-auto py-2 px-1">
           <span>Ngoài khung giờ hoạt động của nhà hàng. Giờ hoạt động: </span>
           <b className="text-red-500">8:30 - 21:00</b>
         </div>
       </div>
       <div className="max-w-screen-lg mx-auto mt-2 px-1 pb-1">
-        <div className="flex gap-2 flex-wrap">
+        <div className="flex gap-2 overflow-x-auto">
           <Button variant={"secondary"} className="font-medium">
             Tất cả
           </Button>
